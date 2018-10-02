c6f9076a8d0d0078343a9b45f9b4448b6319692e
Neha Chaudhary <neha.chaudhary@daemonsolutions.com>
2018-10-02 10:09:03 +0100

    Remove unused import

--- src/main/java/com/neha/weather_test/client/WeatherClient.java
+++ src/main/java/com/neha/weather_test/client/WeatherClient.java
@@ -1,7 +1,6 @@
 package com.neha.weather_test.client;
 
 import com.neha.weather_test.model.WeatherData;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
